dd497c36e74012e723a6f2226f026c9ff1723aa7
= <=>
2022-12-10 23:09:21 -0500

    Primer intento de llamar la función validator

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+/* eslint-disable no-unused-vars */
 import validator from './validator.js';
 // para el uso de querySelector como es una clase se debe poner . 
 //let nameCard=document.querySelector('.card_details-name');
 // si es un ID se pone #
 //const inputName=document.querySelector('#cardholder')
+// eslint-disable-next-line no-unused-vars
 const inputName=document.querySelector('#cardholder');
 const inputNumber=document.querySelector('#cardNumber');
 const inputMonth=document.querySelector('#cardMonth');
 const inputYear=document.querySelector('#cardYear');
 const numberErrorDiv=document.querySelector('.form__cardnumber--error');
 const buttonCheck=document.querySelector('.form__submit');
-
+const concatenar="";
 
 inputNumber.addEventListener('input', ()=> {
   const regExp=/[a-z]/g;
@@ -22,6 +24,11 @@ inputNumber.addEventListener('input', ()=> {
   }
 }); 
 
+
+inputNumber.addEventListener('input', ()=>{
+  if(validator.isValid(concatenar)===false){ numberErrorDiv.textContent="Tu tarjeta no es válida"}
+  else{numberErrorDiv.textContent="Tu tarjeta es como sea válida"}
+});
 buttonCheck.addEventListener('click', ()=>{
 
 });
